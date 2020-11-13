eadd0b17327193962b863cc28f205089d5947ace
Huy Tran <viethuy99lc@gmail.com>
2020-11-13 12:46:27 +0700

    12:46

--- hash.cpp
+++ hash.cpp
@@ -3,14 +3,7 @@ using namespace std;
 
 #define ll long long
 
-ll n, mid1[500005], mid2[500005], res, openp[500005], closep[500005];
-string s;
-
-struct pairto() {
-  ll vt,
-};
-
-struct HashSolve{
+struct HashSolve {
   ll n, base, mod, pow[500005], hs[500005], hs_rev[500005];
   string s;
 
@@ -43,105 +36,10 @@ struct HashSolve{
   }
 } p[2];
 
-bool check_mid2(int st, int mid) {
-  return p[0].getHas(st - mid + 1, st) == p[0].getHasRev(st + 1, st + mid)
-  && p[1].getHas(st - mid + 1, st) == p[1].getHasRev(st + 1, st + mid) ;
-}
 
-bool check_mid1(int st, int mid) {
-  return p[0].getHas(for (ll st = 1; st <= n; st++) {
-    int l = 1, r = min(st, n - st + 1), tmp = 1;
-    while (l <= r) {
-      ll mid = (l + r) >> 1;
-      if (check_mid1(st, mid)) {
-        tmp = mid;
-        l = mid + 1;
-      }
-      else r = mid - 1;
-    }
-    mid1[st] = tmp;
-  }st - mid + 1, st) == p[0].getHasRev(st, st + mid - 1)
-  && p[1].getHas(st - mid + 1, st) == p[1].getHasRev(st, st + mid - 1) ;
-}
-
-void init_mid() {
+int main() {
+  int n = 1;
+  string s = " a";
   p[0].init(n, s, 311, 1e9 + 7);
-//  p[1].init(n, s,
-  for (ll st = 1; st <= n; st++) {
-    int l = 1, r = min(st, n - st + 1), tmp = 1;
-    while (l <= r) {
-      ll mid = (l + r) >> 1;
-      if (check_mid1(st, mid)) {
-        tmp = mid;
-        l = mid + 1;
-      }
-      else r = mid - 1;
-    }
-    mid1[st] = tmp;
-  }401, 1e9 + 103);
-
-  for (ll st = 1; st <= n; st++) {
-    int l = 1, r = min(st, n - st + 1), tmp = 1;
-    while (l <= r) {
-      ll mid = (l + r) >> 1;
-      if (check_mid1(st, mid)) {
-        tmp = mid;
-        l = mid + 1;
-      }
-      else r = mid - 1;
-    }
-    mid1[st] = tmp;
-  }
-
-  for (ll st = 1 ; st < n; st++) {
-    int l = 1, r = min(st, n - st), tmp = 0;
-    while (l <= r) {
-      ll mid = (l + r) >> 1;
-      if (check_mid2(st, mid)) {
-        tmp = mid;
-        l = mid + 1;
-      }
-      else r = mid - 1;
-    }
-    mid2[st] = tmp;
-  }
-}
-
-
-void init_arr() {
-  for (int i = 1; i <= n; i++) {
-    openp[i - mid1[i] + 1] = {mid1[i] , 1}
-  }
-}
-
-
-
-void Solve(){
-  cin >> n >> s;
-  s = " " + s;
-  init_mid();
-  init_tree();
-  for (int i = 1; i <= n; i++) cout << tree_open.get(1, 1, n, i, i) << " "; cout << endl;
-  for (int i = 1; i <= n; i++) cout << tree_close.get(1, 1, n, i, i) << " "; cout << endl;
-  for (int i = 1; i < n; i++) {
-    ll  a = min(tree_close.get(1, 1, n, i, i), tree_open.get(1, 1, n, i + 1, i + 1));
-    res += a;
-    cout << tree_close.get(1, 1, n, i, i) <<" " << tree_open.get(1, 1, n, i + 1, i + 1) << " " << a << endl;
-  }
-  cout << res;
-}
-
-
-int main(){
-    ios::sync_with_stdio(0);cin.tie(0);
-
-    freopen("1.txt","r",stdin);
-//    freopen("1.out","w",stdout);
-
-    int Test_numbers=1;
-//    cin>>Test_numbers;
-
-    while(Test_numbers--) Solve();
-
-	return 0;
+  return 0;
 }
